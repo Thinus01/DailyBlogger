0475562eb30acfde8f2985e483249caef0250415
Thinus Van De Venter <thinusvdv2001@gmail.com>
2023-06-19 10:48:40 +0200

    fix problems for postcontroller

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,7 @@ Rails.application.routes.draw do
   get 'users:user_id/posts:id', to: 'posts#show', as: :user_post
   get '/posts/new', to: 'posts#new', as: :new_user_post
   post '/posts/new', to: 'posts#create'
-  post '/posts/:post_id/comments', to: 'comments#create', as: :new_user_comment
-  get '/posts/:post_id/comments/new', to: 'comments#new', as: :new_post_comment
+  post 'comments/new', to: 'comments#create', as: :new_user_comment
   post 'likes/new', to: 'likes#create', as: :new_user_like
   get 'users/:user_id/post/:id' => 'comments#new', as: 'user_comment'
-  end
-  
+end
